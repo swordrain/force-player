7f64f883008b6423fd0cf3f5bb85425765839fd5
lianli <lianlitongji2002@163.com>
2019-08-10 10:09:56 +0800

    add track progress

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Track/>
+    <Track :progress="30"/>
   </div>
 </template>
 
--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="container" >
+  <div class="container">
     <div class="background-track"></div>
+    <div class="progress" :style="{ width: progress + '%' }"></div>
   </div>
 </template>
 <script>
 export default {
   props: {
-    // disabled: {
-    //   type: Boolean,
-    //   default: false
-    // }
+    progress: {
+      type: Number,
+      default: 50,
+      validator: function(value) {
+        return value >= 0 && value <= 100;
+      }
+    }
   }
 };
 </script>
@@ -25,6 +29,17 @@ export default {
   bottom: 0;
   height: 5px;
   border-radius: 3px;
-  background-color: #E4E7ED;
+  background-color: #e4e7ed;
+  z-index: 1000;
+}
+.progress {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  height: 5px;
+  border-radius: 3px;
+  background-color: #409eff;
+  z-index: 2000;
 }
 </style>
