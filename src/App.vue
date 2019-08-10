06aae955b0f3751899032634cf662cd760a688d6
lianli <lianlitongji2002@163.com>
2019-08-10 15:26:25 +0800

    cover & play button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <force-player>
+    <force-player cover="/cover.gif" width="640px" height="360px">
       <source src="/sample.mp4" type="video/mp4" />
     </force-player>
   </div>
@@ -21,7 +21,6 @@ export default {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   padding: 50px;
 }
--- src/components/ForcePlayer.vue
+++ src/components/ForcePlayer.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="force-player-container">
-    <video :height="height" :width="width">
+    <img v-if="initial" class="force-player-container-cover" :src="cover" />
+    <div v-if="initial" class="force-player-playbtn" @click="startPlay">▷</div>
+    <video ref="video" :height="height" :width="width">
       <slot></slot>
     </video>
   </div>
@@ -15,9 +17,52 @@ export default {
     height: {
       type: String,
       default: "400px"
+    },
+    cover: {
+      type: String
+    }
+  },
+  data: function() {
+    return {
+      initial: true
+    };
+  },
+  methods: {
+    startPlay: function() {
+      this.initial = false;
+      this.$refs.video.play();
     }
   }
 };
 </script>
 <style scoped>
+.force-player-container {
+  position: relative;
+  display: inline-block;
+}
+.force-player-container-cover {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 100;
+}
+.force-player-playbtn {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  color: white;
+  font-size: 40px;
+  border-radius: 50%;
+  border: 1px solid white;
+  width: 60px;
+  height: 60px;
+  text-align: center;
+  line-height: 60px;
+  z-index: 200;
+}
+.force-player-playbtn:hover {
+  cursor: pointer;
+  background: rgba(255, 255, 255, 0.5);
+}
 </style>
