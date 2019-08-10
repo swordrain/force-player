24b29deea1dfd64599fd637a6a48787adbca51fe
lianli <lianlitongji2002@163.com>
2019-08-10 10:01:24 +0800

    initial project&track background

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Track/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Track from './components/Track'
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Track
   }
 }
 </script>
---
+++ src/components/Track.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container" >
+    <div class="background-track"></div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    // disabled: {
+    //   type: Boolean,
+    //   default: false
+    // }
+  }
+};
+</script>
+<style scoped>
+.container {
+  position: relative;
+}
+.background-track {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  height: 5px;
+  border-radius: 3px;
+  background-color: #E4E7ED;
+}
+</style>
